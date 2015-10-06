f88d415c694395aa4e433ada27158f2eb56c753c
Paul Ingles <paul@oobaloo.co.uk>
2015-10-06 10:23:34 +0100

    release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject analytij "0.2.0-SNAPSHOT"
+(defproject analytij "0.2.0"
   :description "Clojure client to interact with Google Analytics API"
   :url "https://github.com/uswitch/analytij"
   :license {:name "Eclipse Public License"
