4aab36ec293dbfa1e226d5cf4f41a8b6cc483d26
Paul Ingles <paul@oobaloo.co.uk>
2017-08-21 09:22:40 +0100

    release 0.4.0 with lazy result pagination

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject analytij "0.3.3"
+(defproject analytij "0.4.0"
   :description "Clojure client to interact with Google Analytics API"
   :url "https://github.com/uswitch/analytij"
   :license {:name "Eclipse Public License"
